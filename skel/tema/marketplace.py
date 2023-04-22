469bab8f63785b8d7da1d6ea439d769d47825f67
Eduard-Costin <eduard_costin2001@yahoo.com>
2023-04-22 20:42:43 +0300

    logging add
    Signed-off-by: Eduard-Costin <eduard_costin2001@yahoo.com>

--- skel/tema/marketplace.py
+++ skel/tema/marketplace.py
@@ -5,8 +5,17 @@ Computer Systems Architecture Course
 Assignment 1
 March 2021
 """
+import logging
+import time
+from logging.handlers import RotatingFileHandler
 from threading import Lock
 
+logging.basicConfig(
+    handlers=[RotatingFileHandler('./marketplace.log', maxBytes=100000, backupCount=5)],
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s: %(message)s"
+)
+logging.Formatter.converter = time.gmtime
 
 class Marketplace:
     """
@@ -34,6 +43,7 @@ class Marketplace:
 
         self.lock = Lock()
 
+
     def register_producer(self):
         """
         Returns an id for the producer that calls this.
@@ -42,7 +52,7 @@ class Marketplace:
             self.number_of_producers += 1
             self.number_of_carts += 1
             self.producers[self.number_of_producers] = []
-
+            logging.info("register prod id = %s", self.number_of_producers)
         return self.number_of_producers
 
     def publish(self, producer_id, product):
@@ -58,12 +68,13 @@ class Marketplace:
         :returns True or False. If the caller receives False, it should wait and then try again.
         """
         if len(self.producers[producer_id]) == self.queue_size_per_producer:
+            logging.info("%s product: %s full", producer_id, product)
             return False
 
-        self.producers[producer_id].append(product)
-
-        self.product_available.append(product[0])
-
+        with self.lock:
+            logging.info("%s product: %s", producer_id, product[0])
+            self.producers[producer_id].append(product)
+            self.product_available.append(product[0])
         return True
 
     def new_cart(self):
@@ -74,6 +85,7 @@ class Marketplace:
         """
         with self.lock:
             self.number_of_carts += 1
+            logging.info("new_cart_id: %s", self.number_of_carts)
             self.carts[self.number_of_carts] = []
 
         return self.number_of_carts
@@ -90,12 +102,13 @@ class Marketplace:
 
         :returns True or False. If the caller receives False, it should wait and then try again
         """
-
         with self.lock:
             for elem in self.product_available:
                 if product == elem:
                     self.carts[cart_id].append(product)
+                    logging.info("cart_id %s: %s", cart_id, product)
                     return True
+        logging.info("%s wait for %s", cart_id, product)
         return False
 
     def remove_from_cart(self, cart_id, product):
@@ -111,9 +124,10 @@ class Marketplace:
         if product in self.carts[cart_id]:
             self.carts[cart_id].remove(product)
             self.product_available.append(product)
-
+            logging.info("remove %s: %s", cart_id, product)
             return True
 
+        logging.info("failed remove %s: %s", cart_id, product)
         return False
 
     def place_order(self, cart_id):
@@ -130,4 +144,5 @@ class Marketplace:
                 if product in list_products_prod:
                     list_products_prod.remove(product)
 
+        logging.info("place_order:%s", list_products)
         return list_products
